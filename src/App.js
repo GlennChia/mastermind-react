2f0afc98a6a3f7f02a56488fd379a2110dbda28f
GlennChia <glennchia7@gmail.com>
2021-07-31 22:09:59 +0800

    feat: add custom orbitron font

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="font-face-orbitron">Mastermind</div>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./fonts/Orbitron/Orbitron-VariableFont_wght.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
