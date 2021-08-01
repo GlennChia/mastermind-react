6892af266f2db80960c8dbe171b2a4e7ba7ed05c
GlennChia <glennchia7@gmail.com>
2021-08-01 09:51:54 +0800

    feat: add dropdown to select model

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import classes from "./index.module.css";
 
-export const Controls = () => {
+export const Controls = ({ setModel, models }) => {
   return (
     <div className={classes.Overall}>
       <div className={classes.Title}>Mastermind</div>
       <div className={classes.Controls}>
         <div className={classes.Title}>AI Mode:</div>
+        <select onChange={(e) => setModel(e.target.value)}>
+          {models.map((model) => (
+            <option key={model} value={model}>
+              {model}
+            </option>
+          ))}
+        </select>
         <div className={classes.Title}>Answer:</div>
       </div>
     </div>
--- src/containers/Game/index.js
+++ src/containers/Game/index.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./index.module.css";
 import { Controls } from "../../components/Controls";
 import { Board } from "../../components/Board";
 
+const MODELS = ["Naive", "Deep Q Learning", "Q Learning"];
+
 export const Game = () => {
+  const [model, setModel] = useState(MODELS[0]);
+
   return (
     <div className={classes.App}>
       <header className={classes.Appheader}>
         <Board />
-        <Controls />
+        <Controls setModel={setModel} models={MODELS} />
         <Board />
       </header>
     </div>
